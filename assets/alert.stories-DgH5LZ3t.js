c722ca3fb8f3013395983ae54269fe5593d7e45d
Simon Stone <simon.stone@globacap.com>
2025-03-20 11:14:39 +0000

    Update
    Signed-off-by: Simon Stone <simon.stone@globacap.com>

---
+++ assets/alert.stories-DgH5LZ3t.js
@@ -0,0 +1,39 @@
+import{j as t,a as l}from"./jsx-runtime-CHAcsyq4.js";import{A as a,a as x}from"./alert-title-CN1cE6_e.js";import{s as C}from"./generateUtilityClasses-7uZHDCF9.js";import"./index-CO9pbFv1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./complete-DfKDyCth.js";import"./in-progress-BhDVRM7c.js";import"./not-started-C2GCbdX0.js";import"./edit-CoTvdx8D.js";import"./chevron-CuO2MovR.js";import"./tick-D-0rNWa6.js";import"./tick-circle-DEx8Rvru.js";import"./info-outlined-BO35JMI8.js";import"./check-circle-N73EjHKU.js";import"./error-outlined-Cly8x-70.js";import"./index-BdESLZbP.js";import"./cross-CurIqIFj.js";import"./upload-CswRUNEI.js";import"./trash-vMOCOH0P.js";import"./cog-outlined-C34frKMW.js";import"./warning-DrjxdJ9G.js";import"./DefaultPropsProvider-Qc2xy7DK.js";import"./useSlot-CRQn5Xiz.js";import"./resolveComponentProps-R-KJbVW4.js";import"./useForkRef-DZzpmK-L.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./alertClasses-D0xC1ff2.js";import"./createSvgIcon-D-0vJS9S.js";import"./capitalize-my3DsAjr.js";import"./Close-Bk6UzDMS.js";import"./Paper-C6YV2L4g.js";import"./useTheme-aPSyNP_P.js";import"./useTheme-CzbpGgWX.js";import"./IconButton-AbnUAF06.js";import"./ButtonBase-spyuHJhX.js";import"./emotion-react.browser.esm-D_d3rkR8.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-D-IwIA3q.js";import"./assertThisInitialized-LnzLPM7_.js";import"./useEnhancedEffect-l_FsEN0U.js";import"./isFocusVisible-B8k4qzLc.js";import"./useId-Dw7xLATF.js";import"./CircularProgress-DQj5DzX1.js";import"./Typography-pWh98bkc.js";import"./index-C_yWRM1m.js";import"./extendSxProp-o9rhGgwT.js";const ye={component:a,title:"Components/Alert",argTypes:{severity:{options:["error","info","success","warning"],control:{type:"radio"}},color:{options:["error","info","success","warning",void 0],control:{type:"select"}}},args:{}},r=({onClose:n,...e})=>t(a,{...e,children:"Please contact our support team if you wish to create equity placement deal with another security type."});r.args={severity:"info",sx:{width:"386px"}};const q=C("ol")`
+  padding-left: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+  list-style: inside decimal;
+  width: 511px;
+`,o=({onClose:n,...e})=>l(a,{...e,children:[t(x,{children:"Instructions"}),l(q,{children:[t("li",{children:"Upload a PDF copy of the NDA."}),t("li",{children:"Assign any fields that the investor must complete. Any additional signatures required (i.e. on behalf of Issuer Name) should be added to the document before it is uploaded to the platform."}),t("li",{children:"You will receive a notification via email whenever an investor signs the NDA."})]})]});o.args={severity:"info",sx:{width:"564px"}};const i=({onClose:n,...e})=>t(a,{...e,children:"Please contact our support team if you wish to create equity placement deal with another security type."});i.args={severity:"warning",sx:{width:"386px"}};const s=({onClose:n,...e})=>t(a,{...e,children:"Please contact our support team if you wish to create equity placement deal with another security type."});s.args={severity:"info"};var p,m,c;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  onClose,
+  ...rest
+}) => <Alert {...rest}>
+    Please contact our support team if you wish to create equity placement deal with another
+    security type.
+  </Alert>`,...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,u,h;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`({
+  onClose,
+  ...rest
+}) => <Alert {...rest}>
+    <AlertTitle>Instructions</AlertTitle>
+    <StyledOl>
+      <li>Upload a PDF copy of the NDA.</li>
+      <li>
+        Assign any fields that the investor must complete. Any additional signatures required (i.e.
+        on behalf of Issuer Name) should be added to the document before it is uploaded to the
+        platform.
+      </li>
+      <li>You will receive a notification via email whenever an investor signs the NDA.</li>
+    </StyledOl>
+  </Alert>`,...(h=(u=o.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var y,f,g;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`({
+  onClose,
+  ...rest
+}) => <Alert {...rest}>
+    Please contact our support team if you wish to create equity placement deal with another
+    security type.
+  </Alert>`,...(g=(f=i.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var w,A,v;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`({
+  onClose,
+  ...rest
+}) => <Alert {...rest}>
+    Please contact our support team if you wish to create equity placement deal with another
+    security type.
+  </Alert>`,...(v=(A=s.parameters)==null?void 0:A.docs)==null?void 0:v.source}}};const fe=["NoTitle","Title","Warning","FullWidth"];export{s as FullWidth,r as NoTitle,o as Title,i as Warning,fe as __namedExportsOrder,ye as default};
---
+++ assets/form-control.stories-D-w9EFSY.js
@@ -0,0 +1,151 @@
+import{a as o,F as _r,j as r}from"./jsx-runtime-CHAcsyq4.js";import{C as Hr}from"./checkbox-BVAYQxx3.js";import{R as qr,F as m}from"./form-control-label-C-iAHR2a.js";import{F as n,a as t}from"./form-label-4wbkqJxy.js";import{I as i}from"./input-B2hw29QA.js";import{N as zr}from"./number-input-W52VZWQD.js";import{C as Jr}from"./currency-input-HDEgtlym.js";import{R as A}from"./radio-BH18ac4x.js";import{S as Kr}from"./select-DkO_aVRQ.js";import{S as Qr}from"./switch-CIM9yH7c.js";import{F as a}from"./form-control-Co61zMSX.js";import"./index-CO9pbFv1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./DefaultPropsProvider-Qc2xy7DK.js";import"./generateUtilityClasses-7uZHDCF9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-D-IwIA3q.js";import"./SwitchBase-Ds3UFRdi.js";import"./useFormControl-Dpmr7vsl.js";import"./ButtonBase-spyuHJhX.js";import"./emotion-react.browser.esm-D_d3rkR8.js";import"./assertThisInitialized-LnzLPM7_.js";import"./useForkRef-DZzpmK-L.js";import"./useEnhancedEffect-l_FsEN0U.js";import"./isFocusVisible-B8k4qzLc.js";import"./useControlled-YZkScxzg.js";import"./capitalize-my3DsAjr.js";import"./createSvgIcon-D-0vJS9S.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./useId-Dw7xLATF.js";import"./FormGroup-BLMNI0WR.js";import"./formControlState-Dq1zat_P.js";import"./FormControlLabel-CGobVcaz.js";import"./useSlot-CRQn5Xiz.js";import"./resolveComponentProps-R-KJbVW4.js";import"./Typography-pWh98bkc.js";import"./index-C_yWRM1m.js";import"./extendSxProp-o9rhGgwT.js";import"./useTheme-CzbpGgWX.js";import"./useThemeProps-DxVlPEjg.js";import"./FormLabel-CWh9BxH-.js";import"./formLabelClasses-kwtaHi0q.js";import"./OutlinedInput-eTQ0icg1.js";import"./isHostComponent-DVu5iVWx.js";import"./utils-DoM3o7-Q.js";import"./ownerWindow-DIR61fab.js";import"./InputAdornment-O-UyIRY6.js";import"./createChainedFunction-BO_9K8Jh.js";import"./complete-DfKDyCth.js";import"./in-progress-BhDVRM7c.js";import"./not-started-C2GCbdX0.js";import"./edit-CoTvdx8D.js";import"./chevron-CuO2MovR.js";import"./tick-D-0rNWa6.js";import"./tick-circle-DEx8Rvru.js";import"./info-outlined-BO35JMI8.js";import"./check-circle-N73EjHKU.js";import"./error-outlined-Cly8x-70.js";import"./index-BdESLZbP.js";import"./cross-CurIqIFj.js";import"./upload-CswRUNEI.js";import"./trash-vMOCOH0P.js";import"./cog-outlined-C34frKMW.js";import"./warning-DrjxdJ9G.js";import"./button-BaC6Xu-N.js";import"./Button-BpBMJraz.js";import"./buttonClasses-B-6mKdQJ.js";import"./resolveProps-CxWqPvcr.js";import"./CircularProgress-DQj5DzX1.js";import"./icon-button-BPrKlY2x.js";import"./IconButton-AbnUAF06.js";import"./MenuItem-Ds4GGZ19.js";import"./List-uWV7A8aN.js";import"./dividerClasses-BHVrBds-.js";import"./divider-C3wLCXTj.js";import"./Divider-lvjRnNp5.js";import"./Select-BF9Bh-BK.js";import"./Menu-DbFho7MW.js";import"./index-A5tg15ia.js";import"./Modal-BlrxFmu4.js";import"./useTheme-aPSyNP_P.js";import"./utils-D4Zmbii6.js";import"./index-BT32HCm8.js";import"./Paper-C6YV2L4g.js";import"./useSlotProps-DyCo3d2M.js";import"./Box-XHGtQXL3.js";import"./FormControl-BYpCVQg4.js";import"./isMuiElement-CAFVGe91.js";import"./alertClasses-D0xC1ff2.js";const Ao={component:a,title:"Components/Forms/FormControl"},R=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(i,{id:"name",placeholder:"Name"})]}),f=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(i,{id:"name",placeholder:"Name"})]}),F=R.bind({}),h=f.bind({}),l=R.bind({});l.args={optional:!0};const s=f.bind({});s.args={optional:!0};const p=R.bind({});p.args={disabled:!0};const c=f.bind({});c.args={disabled:!0};const d=R.bind({});d.args={disabled:!0,optional:!0};const u=f.bind({});u.args={disabled:!0,optional:!0};const b=()=>o(_r,{children:[o(a,{children:[r(n,{htmlFor:"name",children:"Name"}),r(i,{id:"name",placeholder:"Name"})]}),o(a,{children:[r(n,{htmlFor:"age",children:"Age"}),r(i,{id:"age",placeholder:"Age"})]})]}),g=()=>o(_r,{children:[o(a,{children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(i,{id:"name",placeholder:"Name"})]}),o(a,{children:[r(n,{htmlFor:"name",children:"Age"}),r(t,{children:"How old you are"}),r(i,{id:"age",placeholder:"Age"})]})]}),C=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Age"}),r(t,{children:"Example description"}),r(zr,{id:"age",placeholder:"Age"})]}),D=e=>o(a,{disabled:!0,...e,children:[r(n,{htmlFor:"age",children:"Age"}),r(t,{children:"Example description"}),r(zr,{id:"age",placeholder:"Age"})]}),L=e=>o(a,{...e,children:[r(n,{htmlFor:"price",children:"Price"}),r(t,{children:"Example description"}),r(Jr,{id:"price",placeholder:"Price",currency:"GBP"})]}),N=e=>o(a,{disabled:!0,...e,children:[r(n,{htmlFor:"price",children:"Price"}),r(t,{children:"Example description"}),r(Jr,{id:"price",placeholder:"Price",currency:"GBP"})]}),x=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(Kr,{id:"name",placeholder:"Select options",options:[{label:"One",value:"one"},{label:"Two",value:"two"}]})]}),S=e=>o(a,{disabled:!0,...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(Kr,{id:"name",placeholder:"Select options",options:[{label:"One",value:"one"},{label:"Two",value:"two"}]})]}),E=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),o(qr,{id:"name",children:[r(m,{control:r(A,{}),label:"Lorem ipsum",value:"one"}),r(m,{control:r(A,{}),label:"Foo bar",value:"two"})]})]}),W=e=>o(a,{disabled:!0,...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),o(qr,{id:"name",children:[r(m,{control:r(A,{}),label:"Lorem ipsum",value:"one"}),r(m,{control:r(A,{}),label:"Foo bar",value:"two"})]})]}),I=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(m,{control:r(Hr,{id:"name"}),label:"Enabled?"})]}),w=e=>o(a,{disabled:!0,...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(m,{control:r(Hr,{id:"name"}),label:"Enabled?"})]}),v=e=>o(a,{...e,children:[r(n,{htmlFor:"name",children:"Name"}),r(t,{children:"Example description"}),r(m,{control:r(Qr,{id:"name"}),label:"Toggle?"})]});var y,G,O;F.parameters={...F.parameters,docs:{...(y=F.parameters)==null?void 0:y.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(O=(G=F.parameters)==null?void 0:G.docs)==null?void 0:O.source}}};var P,T,k;h.parameters={...h.parameters,docs:{...(P=h.parameters)==null?void 0:P.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(k=(T=h.parameters)==null?void 0:T.docs)==null?void 0:k.source}}};var B,M,j;l.parameters={...l.parameters,docs:{...(B=l.parameters)==null?void 0:B.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(j=(M=l.parameters)==null?void 0:M.docs)==null?void 0:j.source}}};var _,H,q;s.parameters={...s.parameters,docs:{...(_=s.parameters)==null?void 0:_.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(q=(H=s.parameters)==null?void 0:H.docs)==null?void 0:q.source}}};var z,J,K;p.parameters={...p.parameters,docs:{...(z=p.parameters)==null?void 0:z.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(K=(J=p.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var Q,U,V;c.parameters={...c.parameters,docs:{...(Q=c.parameters)==null?void 0:Q.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(V=(U=c.parameters)==null?void 0:U.docs)==null?void 0:V.source}}};var X,Y,Z;d.parameters={...d.parameters,docs:{...(X=d.parameters)==null?void 0:X.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(Z=(Y=d.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,rr,er;u.parameters={...u.parameters,docs:{...($=u.parameters)==null?void 0:$.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Input id="name" placeholder="Name" />
+    </FormControl>;
+}`,...(er=(rr=u.parameters)==null?void 0:rr.docs)==null?void 0:er.source}}};var or,ar,nr;b.parameters={...b.parameters,docs:{...(or=b.parameters)==null?void 0:or.docs,source:{originalSource:`() => <>
+    <FormControl>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <Input id="name" placeholder="Name" />
+    </FormControl>
+
+    <FormControl>
+      <FormLabel htmlFor="age">Age</FormLabel>
+      <Input id="age" placeholder="Age" />
+    </FormControl>
+  </>`,...(nr=(ar=b.parameters)==null?void 0:ar.docs)==null?void 0:nr.source}}};var tr,mr,ir;g.parameters={...g.parameters,docs:{...(tr=g.parameters)==null?void 0:tr.docs,source:{originalSource:`() => <>
+    <FormControl>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Input id="name" placeholder="Name" />
+    </FormControl>
+
+    <FormControl>
+      <FormLabel htmlFor="name">Age</FormLabel>
+      <FormDescription>How old you are</FormDescription>
+      <Input id="age" placeholder="Age" />
+    </FormControl>
+  </>`,...(ir=(mr=g.parameters)==null?void 0:mr.docs)==null?void 0:ir.source}}};var lr,sr,pr;C.parameters={...C.parameters,docs:{...(lr=C.parameters)==null?void 0:lr.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Age</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <NumberInput id="age" placeholder="Age" />
+    </FormControl>;
+}`,...(pr=(sr=C.parameters)==null?void 0:sr.docs)==null?void 0:pr.source}}};var cr,dr,ur;D.parameters={...D.parameters,docs:{...(cr=D.parameters)==null?void 0:cr.docs,source:{originalSource:`args => {
+  return <FormControl disabled {...args}>
+      <FormLabel htmlFor="age">Age</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <NumberInput id="age" placeholder="Age" />
+    </FormControl>;
+}`,...(ur=(dr=D.parameters)==null?void 0:dr.docs)==null?void 0:ur.source}}};var Fr,hr,br;L.parameters={...L.parameters,docs:{...(Fr=L.parameters)==null?void 0:Fr.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="price">Price</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <CurrencyInput id="price" placeholder="Price" currency="GBP" />
+    </FormControl>;
+}`,...(br=(hr=L.parameters)==null?void 0:hr.docs)==null?void 0:br.source}}};var gr,Cr,Dr;N.parameters={...N.parameters,docs:{...(gr=N.parameters)==null?void 0:gr.docs,source:{originalSource:`args => {
+  return <FormControl disabled {...args}>
+      <FormLabel htmlFor="price">Price</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <CurrencyInput id="price" placeholder="Price" currency="GBP" />
+    </FormControl>;
+}`,...(Dr=(Cr=N.parameters)==null?void 0:Cr.docs)==null?void 0:Dr.source}}};var Lr,Nr,xr;x.parameters={...x.parameters,docs:{...(Lr=x.parameters)==null?void 0:Lr.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Select id="name" placeholder="Select options" options={[{
+      label: 'One',
+      value: 'one'
+    }, {
+      label: 'Two',
+      value: 'two'
+    }]} />
+    </FormControl>;
+}`,...(xr=(Nr=x.parameters)==null?void 0:Nr.docs)==null?void 0:xr.source}}};var Sr,Er,Wr;S.parameters={...S.parameters,docs:{...(Sr=S.parameters)==null?void 0:Sr.docs,source:{originalSource:`args => {
+  return <FormControl disabled {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <Select id="name" placeholder="Select options" options={[{
+      label: 'One',
+      value: 'one'
+    }, {
+      label: 'Two',
+      value: 'two'
+    }]} />
+    </FormControl>;
+}`,...(Wr=(Er=S.parameters)==null?void 0:Er.docs)==null?void 0:Wr.source}}};var Ir,wr,vr;E.parameters={...E.parameters,docs:{...(Ir=E.parameters)==null?void 0:Ir.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <RadioGroup id="name">
+        <FormControlLabel control={<Radio />} label="Lorem ipsum" value="one" />
+        <FormControlLabel control={<Radio />} label="Foo bar" value="two" />
+      </RadioGroup>
+    </FormControl>;
+}`,...(vr=(wr=E.parameters)==null?void 0:wr.docs)==null?void 0:vr.source}}};var Ar,Rr,fr;W.parameters={...W.parameters,docs:{...(Ar=W.parameters)==null?void 0:Ar.docs,source:{originalSource:`args => {
+  return <FormControl disabled {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <RadioGroup id="name">
+        <FormControlLabel control={<Radio />} label="Lorem ipsum" value="one" />
+        <FormControlLabel control={<Radio />} label="Foo bar" value="two" />
+      </RadioGroup>
+    </FormControl>;
+}`,...(fr=(Rr=W.parameters)==null?void 0:Rr.docs)==null?void 0:fr.source}}};var yr,Gr,Or;I.parameters={...I.parameters,docs:{...(yr=I.parameters)==null?void 0:yr.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <FormControlLabel control={<Checkbox id="name" />} label="Enabled?" />
+    </FormControl>;
+}`,...(Or=(Gr=I.parameters)==null?void 0:Gr.docs)==null?void 0:Or.source}}};var Pr,Tr,kr;w.parameters={...w.parameters,docs:{...(Pr=w.parameters)==null?void 0:Pr.docs,source:{originalSource:`args => {
+  return <FormControl disabled {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <FormControlLabel control={<Checkbox id="name" />} label="Enabled?" />
+    </FormControl>;
+}`,...(kr=(Tr=w.parameters)==null?void 0:Tr.docs)==null?void 0:kr.source}}};var Br,Mr,jr;v.parameters={...v.parameters,docs:{...(Br=v.parameters)==null?void 0:Br.docs,source:{originalSource:`args => {
+  return <FormControl {...args}>
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormDescription>Example description</FormDescription>
+      <FormControlLabel control={<Switch id="name" />} label="Toggle?" />
+    </FormControl>;
+}`,...(jr=(Mr=v.parameters)==null?void 0:Mr.docs)==null?void 0:jr.source}}};const Ro=["Default","WithDescription","Optional","OptionalWithDescription","Disabled","DisabledWithDescription","OptionalDisabled","OptionalDisabledWithDescription","Multiple","MultipleWithDescription","WithNumberInput","WithNumberInputDisabled","WithCurrencyInput","WithCurrencyInputDisabled","WithSelect","WithSelectDisabled","WithRadioGroup","WithRadioGroupDisabled","WithCheckbox","WithCheckboxDisabled","WithSwitch"];export{F as Default,p as Disabled,c as DisabledWithDescription,b as Multiple,g as MultipleWithDescription,l as Optional,d as OptionalDisabled,u as OptionalDisabledWithDescription,s as OptionalWithDescription,I as WithCheckbox,w as WithCheckboxDisabled,L as WithCurrencyInput,N as WithCurrencyInputDisabled,h as WithDescription,C as WithNumberInput,D as WithNumberInputDisabled,E as WithRadioGroup,W as WithRadioGroupDisabled,x as WithSelect,S as WithSelectDisabled,v as WithSwitch,Ro as __namedExportsOrder,Ao as default};
